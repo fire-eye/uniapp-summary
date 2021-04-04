4d55ab423f1a6024d9df5e95e0904dc07ad8ad58
leon <419299210@qq.com>
2021-04-04 13:09:51 +0800

    优化结构，新增vue教程案例

--- apis/component.js
+++ apis/component.js
@@ -3,43 +3,43 @@ const lists = [
 	{
 		title : '地图',
 		desc : '简介',
-		link : '/pages/tabbar/map/map',
+		link : '/pages/components/map/map',
 		icon : 'lIcon-map-1'
 	},
 	{
 		title : '图标/动画',
 		desc : '简介',
-		link : '/pages/tabbar/icon/icon',
+		link : '/pages/components/icon/icon',
 		icon : 'lIcon-icon'
 	},
 	{
 		title : '富文本',
 		desc : '多端兼容富文本组件,支持图片预览、视频播放、表格渲染等',
-		link : '/pages/tabbar/text-rich/text-rich',
+		link : '/pages/components/text-rich/text-rich',
 		icon : 'lIcon-rich'
 	},
 	{
 		title : '图表',
 		desc : '简介',
-		link : '/pages/tabbar/echat/echat',
+		link : '/pages/components/echat/echat',
 		icon : 'lIcon-chart-1'
 	},
 	{
 		title : '海报',
 		desc : '简介',
-		link : '',
+		link : '/pages/components/canvas/canvas',
 		icon : 'lIcon-poster'
 	},
 	{
 		title : '视频',
 		desc : '视屏功能应用案例,包含弹幕与弹幕发布',
-		link : '/pages/tabbar/video/video',
+		link : '/pages/components/video/video',
 		icon : 'lIcon-draw'
 	},
 	{
 		title : '抽奖',
 		desc : '简介',
-		link : '/pages/tabbar/luck-draw/demo',
+		link : '/pages/components/luck-draw/demo',
 		icon : 'lIcon-draw'
 	},
 	{
